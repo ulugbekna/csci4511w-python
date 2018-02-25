1b432752f6faf476481b4f1334c7ac215ea9d30d
Ulugbek Abdullaev <abdul359@umn.edu>
2018-02-24 20:09:20 -0600

    minor changes

--- Constraint Satisfaction Problem/solve_cryptoarith_puzzle.py
+++ Constraint Satisfaction Problem/solve_cryptoarith_puzzle.py
@@ -1,3 +1,11 @@
+'''
+solving_cryptoarith_puzzle.py solves cryptoarithmetic puzzles, e.g. send+more=money, where each
+letter is uniquely mapped to a digit. The program was encouraged by "Artificial Intelligence: A Modern Approach" by
+Peter Norvig and Stuart Russell and Peter Norvig's "Solving Every Sudoku Puzzle".
+'''
+
+import time
+
 # input
 addend_1 = input('1st addend: ')
 addend_2 = input('2nd addend: ')
@@ -41,8 +49,9 @@ for i in sp_wrd_lst:
 # build an initial dictionary
 values = {}
 for l in set_of_letters:
-    values[l] = '0123456789'
+    values[l] = '1234567890'
 # print(values)
+
 
 # tip: str.replace(old, new) returns a string, does not manipulate the string itself
 # eliminate val in values[letter]
@@ -69,6 +78,7 @@ def eliminate(values, letter, val):
     return values
 
 
+# assigns each letter a value
 def assign(values, letter, val):
     other_values = values[letter].replace(val, '')
     if all(eliminate(values, letter, tmp_val) for tmp_val in other_values):
@@ -77,6 +87,7 @@ def assign(values, letter, val):
         return False
 
 
+# turns a string word into an int based on each letter's value in values
 def wrd_to_int(word, values):
     tmp = 0
     for s in word:
@@ -85,7 +96,7 @@ def wrd_to_int(word, values):
 # print(wrd_to_int('money', eliminate(solution, '', '012345678')))
 
 
-# check
+# check if the values that we have found fits the equality
 def check_sum(a1, a2, s, values):
     if wrd_to_int(a1, values) + wrd_to_int(a2, values) == wrd_to_int(s, values):
         print("true")
@@ -94,18 +105,20 @@ def check_sum(a1, a2, s, values):
         return False
 
 
+# search function
 def search(values):
     if values is False:
-        return False  # Failed earlier
-    if all(len(values[s]) == 1 for s in set_of_letters):
+        return False
+    if all(len(values[l]) == 1 for l in set_of_letters):
         if check_sum(addend_1, addend_2, summand, values):
-            return values  # Solved!
+            return values
         else:
             return False
-    n,s = min((len(values[s]), s) for s in set_of_letters if len(values[s]) > 1)
-    return some(search(assign(values.copy(), s, d)) for d in values[s])
+    n,l = min((len(values[l]), l) for l in set_of_letters if len(values[l]) > 1)
+    return some(search(assign(values.copy(), l, v)) for v in values[l])
 
 
+# returns first non-false element in sequence
 def some(seq):
     for e in seq:
         if e:
@@ -113,20 +126,27 @@ def some(seq):
     return False
 
 
-def in_grid(values):
-    if values == False:
+# prints pretty output
+def pretty_print(values):
+    if values is False:
         print('No solution found. Sorry.')
     else:
         print(addend_1 + '+' + addend_2 + '=' + summand)
         print(str(wrd_to_int(addend_1, values)) + '+' + str(wrd_to_int(addend_2, values)) + '=' + str(wrd_to_int(summand, values)))
 
 
+# if the summand is one letter more than both of addends then its left-most letter must be 1
 if len(summand) - 1 == len(addend_1) and len(summand) - 1 == len(addend_2):
     assign(values, summand[0], '1')
 
-in_grid(search(values))
+
+# search itself
+start = time.time()
+pretty_print(search(values))
+end = time.time()
+print('time elapsed: ' + str(end - start) + ' seconds')
+
 
 # todo: leading letters should not be 0's
-# todo: in case summand has more letters than both addends, the leading letter of the summand should equal 1 (?)
 # todo: how to make it faster (?)
 
